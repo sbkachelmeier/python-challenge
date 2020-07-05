04c454b87b0ccd18b325bc1eadacf4ad1055e6a8
Sarah Kachelmeier <sbkachelmeier@gmail.com>
2020-07-05 18:14:20 -0500

    Calculated results

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,18 +9,45 @@ with open(file) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 #Read the header row
     csv_header = next(csv_reader)
-#Calculate total number of months
-    months = len(list(csv_reader))
-    print(months)
+#Initialize variables
+    total_months = 0
+    total_pl = 0
+    greatest_increase = 0 
+    greatest_month = str()
+    greatest_decrease = 0 
+    decrease_month = str()
+    previous_row = None
+    average_change = 0
+    total_change = 0
+    
+#Calculate number of months
+    for row in csv_reader:
+        total_months += 1
     
 #Calculate total profit/losses
-    # total = 0
-    # for row in csv.reader(file)):
-    #     total = row[1] + total
-        
-        #print(total)
-#Calculate greatest change
+        total_pl = int(row[1]) + total_pl
+#Calculate average change
+        if previous_row != None:
+            total_change += int(row[1]) - int(previous_row)
+
 #Calculate greatest increase
-    greatest_increase = max
-#Calculate greatest decrease
+            if int(row[1]) - int(previous_row) > greatest_increase:
+                greatest_increase = int(row[1]) - int(previous_row) 
+   #Calculate greatest decrease
+            if int(row[1]) - int(previous_row) < greatest_decrease:
+                greatest_decrease = int(row[1]) - int(previous_row) 
+        previous_row = int(row[1])
+    average_change = total_change / (total_months-1)
+        
+     
+    
+
+#Print the results!
+    print("Financial Analysis")
+    print("-------------------------")
+    print(total_months)
+    print(average_change)
+    print(total_pl)
+    print(greatest_increase)
+    print(greatest_decrease)
     
